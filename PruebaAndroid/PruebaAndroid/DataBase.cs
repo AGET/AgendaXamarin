4f0dc02e7960d7f9bda9c6d2460b7314d47f429b
Aldo Gamaliel Estrada Tepec <aldoget1992@gmail.com>
2015-12-11 11:27:12 -0600

    Primero Version

---
+++ PruebaAndroid/PruebaAndroid/DataBase.cs
@@ -0,0 +1,175 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Database.Sqlite;
+using System.IO; 
+
+namespace PruebaAndroid
+{
+	/*[BroadcastReceiver]
+	public class DataBase : BroadcastReceiver
+	{*/
+	class Database
+	{
+		//SQLiteDatabase object for database handling
+		private SQLiteDatabase sqldb;
+		// Cadena para el manejo de consultas 
+		private string sqldb_query;
+		// Cadena para la manipulación del mensaje 
+		private string sqldb_message;
+		// Booleano para comprobar disponibilidad de base de datos 
+		private bool sqldb_available;
+		// Cero argumento del constructor, inicia una nueva instancia de la clase de base de datos 
+		public Database()
+		{
+			sqldb_message = "";
+			sqldb_available = false;
+		}
+		// Un argumento constructor, inicia una nueva instancia de la clase de base de datos con el parámetro nombre de la base 
+		public Database(string sqldb_name)
+		{
+			try
+			{
+				sqldb_message = "";
+				sqldb_available = false;
+				CreateDatabase(sqldb_name);
+			}
+			catch (SQLiteException ex) 
+			{
+				sqldb_message = ex.Message;
+			}
+		}
+		// Obtiene o establece el valor de base de datos dependiendo de la disponibilidad 
+		public bool DatabaseAvailable
+		{
+			get{ return sqldb_available; }
+			set{ sqldb_available = value; }
+		}
+		// Obtiene o establece el valor para el manejo de mensajes 
+		public string Message
+		{
+			get{ return sqldb_message; }
+			set{ sqldb_message = value; }
+		}
+		// Crea una nueva base de datos que el nombre viene dado por el parámetro 
+		public void CreateDatabase(string sqldb_name)
+		{
+			try
+			{
+				sqldb_message = "";
+				string sqldb_location = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+				string sqldb_path = Path.Combine(sqldb_location, sqldb_name);
+				bool sqldb_exists = File.Exists(sqldb_path);
+				if(!sqldb_exists)
+				{
+					sqldb = SQLiteDatabase.OpenOrCreateDatabase(sqldb_path,null);
+					sqldb_query = "CREATE TABLE IF NOT EXISTS MyTable (_id INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR, LastName VARCHAR, Age INT);";
+					sqldb.ExecSQL(sqldb_query);
+					sqldb_message = "Database: " + sqldb_name + " created";
+				}
+				else
+				{
+					sqldb = SQLiteDatabase.OpenDatabase(sqldb_path, null, DatabaseOpenFlags.OpenReadwrite);
+					sqldb_message = "Database: " + sqldb_name + " opened";
+				}
+				sqldb_available=true;
+			}
+			catch(SQLiteException ex) 
+			{
+				sqldb_message = ex.Message;
+			}
+		}
+		// Añade un nuevo registro con los parámetros dados 
+		public void AddRecord(string sName, string sLastName, int iAge)
+		{
+			try
+			{
+				sqldb_query = "INSERT INTO MyTable (Name, LastName, Age) VALUES ('" + sName + "','" + sLastName + "'," + iAge + ");";
+				sqldb.ExecSQL(sqldb_query);
+				sqldb_message = "Record saved";
+			}
+			catch(SQLiteException ex) 
+			{
+				sqldb_message = ex.Message;
+			}
+		}
+		// Actualiza un registro existente con los parámetros dados en función de parámetro id 
+		public void UpdateRecord(int iId, string sName, string sLastName, int iAge)
+		{
+			try
+			{
+				sqldb_query="UPDATE MyTable SET Name ='" + sName + "', LastName ='" + sLastName + "', Age ='" + iAge + "' WHERE _id ='" + iId + "';";
+				sqldb.ExecSQL(sqldb_query);
+				sqldb_message = "Record " + iId + " updated";
+			}
+			catch(SQLiteException ex)
+			{
+				sqldb_message = ex.Message;
+			}
+		}
+		// Elimina el registro asociado al parámetro id 
+		public void DeleteRecord(int iId)
+		{
+			try
+			{
+				sqldb_query = "DELETE FROM MyTable WHERE _id ='" + iId + "';";
+				sqldb.ExecSQL(sqldb_query);
+				sqldb_message = "Record " + iId + " deleted";
+			}
+			catch(SQLiteException ex) 
+			{
+				sqldb_message = ex.Message;
+			}
+		}
+		// Busca en un registro y devuelve un cursor Android.Database.ICursor 
+		// Muestra todos los registros de la tabla 
+		public Android.Database.ICursor GetRecordCursor()
+		{
+			Android.Database.ICursor sqldb_cursor = null;
+			try
+			{
+				sqldb_query = "SELECT*FROM MyTable;";
+				sqldb_cursor = sqldb.RawQuery(sqldb_query, null);
+				if(!(sqldb_cursor != null))
+				{
+					sqldb_message = "Record not found";
+				}
+			}
+			catch(SQLiteException ex) 
+			{
+				sqldb_message = ex.Message;
+			}
+			return sqldb_cursor;
+		}
+		// Busca en un registro y devuelve un cursor Android.Database.ICursor 
+		// Muestra los registros de acuerdo con los criterios de búsqueda 
+		public Android.Database.ICursor GetRecordCursor(string sColumn, string sValue)
+		{
+			Android.Database.ICursor sqldb_cursor = null;
+			try
+			{
+				sqldb_query = "SELECT*FROM MyTable WHERE " + sColumn + " LIKE '" + sValue + "%';";
+				sqldb_cursor = sqldb.RawQuery(sqldb_query, null);
+				if(!(sqldb_cursor != null))
+				{
+					sqldb_message = "Record not found";
+				}
+			}
+			catch(SQLiteException ex) 
+			{
+				sqldb_message = ex.Message;
+			}
+			return sqldb_cursor;
+		}
+	}
+}
+
---
+++ PruebaAndroid/PruebaAndroid/MainActivity.cs
@@ -0,0 +1,190 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+
+namespace PruebaAndroid
+{
+	// Actividad principal de aplicación de lanzamiento 
+	[Activity (Label = "Agenda", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+	public class MainActivity : Activity
+	{
+		String index ="";
+		// Clase de base de datos nuevo objeto 
+		Database sqldb;
+		// Nombre, Apellidos y Edad EditText para la entrada de datos 
+		EditText txtName, txtAge, txtLastName;
+		// Objeto Message TextView para la visualización de datos
+		//TextView shMsg;
+		// Añadir, Editar, Eliminar y Buscar ImageButton objetos para los eventos de manipulación 
+		ImageButton imgAdd, imgEdit, imgDelete, imgSearch;
+		// Objeto ListView para mostrar los datos de la base de datos 
+		ListView listItems;
+		// Inicia el evento Crear de aplicación 
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			// Establecer nuestro diseño principal como vista predeterminada 
+			SetContentView (Resource.Layout.Main);
+			// Inicializa nueva base de datos objeto de clase 
+			sqldb = new Database("person_db");
+			// Obtiene instancias de objetos ImageButton 
+			imgAdd = FindViewById<ImageButton> (Resource.Id.imgAdd);
+			imgDelete = FindViewById<ImageButton> (Resource.Id.imgDelete);
+			imgEdit = FindViewById<ImageButton> (Resource.Id.imgEdit);
+			imgSearch = FindViewById<ImageButton> (Resource.Id.imgSearch);
+			// Obtiene instancias de objetos EditText 
+			txtAge = FindViewById<EditText> (Resource.Id.txtAge);
+			txtLastName = FindViewById<EditText> (Resource.Id.txtLastName);
+			txtName = FindViewById<EditText> (Resource.Id.txtName);
+			// Obtiene instancias de objeto TextView 
+			//shMsg = FindViewById<TextView> (Resource.Id.shMsg);
+			// Obtiene instancia de objeto ListView 
+			listItems = FindViewById<ListView> (Resource.Id.listItems);
+			// Establece la propiedad clase de mensaje de base de datos a la instancia shMsg TextView 
+			//shMsg.Text = sqldb.Message;
+			// Crea ImageButton clic eventos para imgAdd, ImgEdit, imgDelete y imgSearch 
+			imgAdd.Click += delegate {
+				// Llama a la función addRecord para añadir un nuevo récord 
+				if(txtName.Text != ""){
+				sqldb.AddRecord (txtName.Text, txtLastName.Text, int.Parse (txtAge.Text));
+			//	shMsg.Text = sqldb.Message;
+				Toast.MakeText(this,"Datos agregados",ToastLength.Short).Show();
+				txtName.Text = txtAge.Text = txtLastName.Text = "";
+				GetCursorView();
+				}else{
+					//shMsg.Text = "Ingrese datos";
+					Toast.MakeText(this,"Ingrese datos",ToastLength.Short).Show();
+				}
+			};
+
+			imgEdit.Click += delegate {
+				if(txtName.Text != ""){
+				//int iId = int.Parse(shMsg.Text);
+				int iId = int.Parse(index);
+				// Llama a la función UpdateRecord para la actualización de un registro existente 
+				sqldb.UpdateRecord (iId, txtName.Text, txtLastName.Text, int.Parse (txtAge.Text));
+				//shMsg.Text = sqldb.Message;
+				Toast.MakeText(this,"Editado",ToastLength.Short).Show();
+				txtName.Text = txtAge.Text = txtLastName.Text = "";
+				GetCursorView();
+				}else{
+					//shMsg.Text = "Seleccione un dato";
+					Toast.MakeText(this,"Seleccione un dato",ToastLength.Short).Show();
+				}
+			};
+
+			imgDelete.Click += delegate {
+				if(txtName.Text != ""){
+				//int iId = int.Parse(shMsg.Text);
+				int iId = int.Parse(index);
+				// Llama a la función DeleteRecord para borrar el registro asociado al parámetro id 
+				sqldb.DeleteRecord (iId);
+				//shMsg.Text = sqldb.Message;
+				Toast.MakeText(this,"Eliminado",ToastLength.Short).Show();
+				txtName.Text = txtAge.Text = txtLastName.Text = "";
+				GetCursorView();
+			}else{
+					//shMsg.Text = "Seleccione un dato";
+					Toast.MakeText(this,"Seleccione un dato",ToastLength.Short).Show();
+				}
+			};
+
+			imgSearch.Click += delegate {
+				// Llama a la función GetCursorView para buscar todos los registros o registro único de acuerdo a los criterios de búsqueda 
+				string sqldb_column = "";
+				if (txtName.Text.Trim () != "") 
+				{
+					sqldb_column = "Name";
+					GetCursorView (sqldb_column, txtName.Text.Trim ());
+				} else
+					if (txtLastName.Text.Trim () != "") 
+					{
+						sqldb_column = "LastName";
+						GetCursorView (sqldb_column, txtLastName.Text.Trim ());
+					} else
+						if (txtAge.Text.Trim () != "") 
+						{
+							sqldb_column = "Age";
+							GetCursorView (sqldb_column, txtAge.Text.Trim ());
+						} else 
+						{
+							GetCursorView ();
+							sqldb_column = "en Todos";
+						}
+				//shMsg.Text = "Buscados " + sqldb_column + ".";
+				Toast.MakeText(this, "Buscados " + sqldb_column + ".",ToastLength.Short).Show();
+			};
+			// Agregar controlador de eventos ItemClick a instancia ListView 
+			listItems.ItemClick += new EventHandler<AdapterView.ItemClickEventArgs> (item_Clicked);
+		}
+		// Lanzado cuando un elemento ListView se hace clic 
+		void item_Clicked (object sender, AdapterView.ItemClickEventArgs e)
+		{
+			// Obtiene TextView instancia de objeto de diseño record_view 
+			TextView shId = e.View.FindViewById<TextView> (Resource.Id.Id_row);
+			TextView shName = e.View.FindViewById<TextView> (Resource.Id.Name_row);
+			TextView shLastName = e.View.FindViewById<TextView> (Resource.Id.LastName_row);
+			TextView shAge = e.View.FindViewById<TextView> (Resource.Id.Age_row);
+			// Lee valores y conjuntos de instancias de objetos EditText 
+			txtName.Text = shName.Text;
+			txtLastName.Text = shLastName.Text;
+			txtAge.Text = shAge.Text;
+			// Muestra mensajes para operaciones CRUD 
+			//shMsg.Text = shId.Text;
+			index = shId.Text;
+		}
+		// Obtiene la vista del cursor para mostrar todos los registros 
+		void GetCursorView()
+		{
+			Android.Database.ICursor sqldb_cursor = sqldb.GetRecordCursor ();
+			if (sqldb_cursor != null) 
+			{
+				sqldb_cursor.MoveToFirst ();
+				string[] from = new string[] {"_id","Name","LastName","Age" };
+				int[] to = new int[] {
+					Resource.Id.Id_row,
+					Resource.Id.Name_row,
+					Resource.Id.LastName_row,
+					Resource.Id.Age_row
+				};
+				// Crea un SimplecursorAdapter para el objeto ListView 
+				SimpleCursorAdapter sqldb_adapter = new SimpleCursorAdapter (this, Resource.Layout.record_view, sqldb_cursor, from, to);
+				listItems.Adapter = sqldb_adapter;
+			} 
+			else 
+			{
+				//shMsg.Text = sqldb.Message;
+				Toast.MakeText(this,"No hay contactos",ToastLength.Short).Show();
+			}
+		}
+		// Obtiene la vista del cursor para mostrar los registros de acuerdo con los criterios de búsqueda 
+		void GetCursorView (string sqldb_column, string sqldb_value)
+		{
+			Android.Database.ICursor sqldb_cursor = sqldb.GetRecordCursor (sqldb_column, sqldb_value);
+
+			if (sqldb_cursor != null) 
+			{
+				sqldb_cursor.MoveToFirst ();
+				string[] from = new string[] {"_id","Name","LastName","Age" };
+				int[] to = new int[] 
+				{
+					Resource.Id.Id_row,
+					Resource.Id.Name_row,
+					Resource.Id.LastName_row,
+					Resource.Id.Age_row
+				};
+				SimpleCursorAdapter sqldb_adapter = new SimpleCursorAdapter (this, Resource.Layout.record_view, sqldb_cursor, from, to);
+				listItems.Adapter = sqldb_adapter;
+			} 
+			else 
+			{
+				Toast.MakeText(this,"No hay contactos",ToastLength.Short).Show();
+			}
+		}
+
+	}
+}
+
+
